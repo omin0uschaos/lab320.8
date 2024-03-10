0f5fe72f4cd6b4f1bd0e32e44dc6dae0837a8f91
omin0uschaos <10238707+omin0uschaos@users.noreply.github.com>
2024-03-10 00:49:49 -0800

    set up axios

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import { useState } from 'react'
 import NavBar from './components/NavBar'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import Main from './pages/Main'
+import Animals from './pages/Animals'
+import Characters from './pages/Characters'
+import Episodes from './pages/Episodes'
+import Spaceships from './pages/Spaceships'
+import NotFound from './pages/NotFound'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +15,14 @@ function App() {
   return (
     <>
       <NavBar />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/animals' element={<Animals />} />
+        <Route path='/characters' element={<Characters />} />
+        <Route path='/episodes' element={<Episodes />} />
+        <Route path='/spaceships' element={<Spaceships />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </>
   )
 }
