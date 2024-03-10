ba4f3d972fbf7273a994394f527659bc21458b20
omin0uschaos <10238707+omin0uschaos@users.noreply.github.com>
2024-03-10 00:10:22 -0800

    create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import NavBar from './components/NavBar'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 
@@ -9,7 +8,8 @@ function App() {
 
   return (
     <>
-
+      <NavBar />
+      <div>star trek</div> 
     </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+
+function NavBar() {
+  return (
+    <div className="nav">
+        <Link to='/spaceships'>
+            <div>SPACESHIPS</div>
+        </Link>
+        <Link to='/characters'>
+            <div>CHARACTERS</div>
+        </Link>
+        <div>STAR TREK</div>
+        <Link to='/episodes'>
+            <div>EPISODES</div>
+        </Link>
+        <Link to='/animals'>
+            <div>ANIMALS</div>
+        </Link>
+    </div>
+  )
+}
+
+export default NavBar
