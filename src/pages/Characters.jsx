bfab2828433a8681868beeae5d717ea361220256
omin0uschaos <10238707+omin0uschaos@users.noreply.github.com>
2024-03-10 13:53:39 -0700

    create characters page

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,7 +1,68 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+
 function Characters() {
-  return (
-    <div>Characters</div>
-  )
+    const [characters, setCharacters] = useState(null)
+
+    let { page } = useParams();
+    const url = `https://stapi.co/api/v1/rest/character/search?pageNumber=${page}&pageSize=50`
+
+    async function getCharacters() {
+        try {
+            let res = await axios.get(url);
+
+            setCharacters(res.data);
+            
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getCharacters();
+    }, [page]);
+
+    const loaded = () => {
+      if (characters) { // Check if episodes and episodes.episodes are not null
+          let prevPage = Number(page) - 1;
+          let nextPage = Number(page) + 1;
+  
+          if (prevPage <= 0){
+            let prevPagePath = `/characters/${prevPage}`;
+          }
+          let prevPagePath = `/characters/${prevPage}`;
+          let nextPagePath = `/characters/${nextPage}`;
+  
+          return (
+              <> 
+                  <ul>
+                      {characters.characters.map((character) => (
+                          <li key={character.uid}>
+                              <span>{character.name}</span>
+                              <span>{character.gender}</span>
+                          </li>
+                      ))}
+                  </ul>
+                  <div className="pagination">
+                      <Link to={prevPagePath}>PREV</Link>
+                      <Link to={nextPagePath}>NEXT</Link>
+                  </div>
+              </> 
+          );
+      }
+  };
+    
+
+    const loading = () => {
+        return <h1>Loading...</h1>;
+    };
+
+    return characters ? loaded() : loading();
+
 }
 
-export default Characters
+export default Characters;
